16cd20fedf4ff57c4e4858e7d2831dc68eaf4878
shiblykt <shiblykt56@gmail.com>
2022-04-18 00:04:53 +0530

    seller module updated

--- seller/urls.py
+++ seller/urls.py
@@ -2,4 +2,8 @@ from django.urls import include,path
 from . import views
 urlpatterns = [
     path('',views.landing,name='landing'),
+    path('login',views.login,name='login'),
+    path('registration',views.registration,name='registration'),
+    path('bankInfo',views.bankInfo,name='bankInfo'),
+    path('buisnessInfo',views.buisnessInfo,name='buisnessInfo'),
 ]
--- seller/views.py
+++ seller/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 def landing(request):
     return render(request,'reseller/reseller_landing.html')
+
+def login(request):
+    return render(request,'reseller/reseller_login.html')
+
+def registration(request):
+    return render(request,'reseller/reseller_registration.html')
+
+def bankInfo(request):
+    return render(request,'reseller/reseller_bankdetails.html')
+
+def buisnessInfo(request):
+    return render(request,'reseller/reseller_buisnessInfo.html')
