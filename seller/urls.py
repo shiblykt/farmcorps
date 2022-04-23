f7d1e702fd54aeedd51d187ae9dd6d61b559c28e
shiblykt <shiblykt56@gmail.com>
2022-04-23 22:58:07 +0530

    seller templates added

--- seller/urls.py
+++ seller/urls.py
@@ -6,4 +6,9 @@ urlpatterns = [
     path('registration',views.registration,name='registration'),
     path('bankInfo',views.bankInfo,name='bankInfo'),
     path('buisnessInfo',views.buisnessInfo,name='buisnessInfo'),
+    path('home',views.home,name='sellerHome'),
+    path('add/vegetables',views.addVeg,name='addVeg'),
+    path('edit/vegetables',views.editVeg,name='editVeg'),
+
+
 ]
