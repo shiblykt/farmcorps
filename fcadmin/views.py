e4edb0262fe4906e7e179e11d94b021891e23012
shiblykt <shiblykt56@gmail.com>
2022-04-27 13:43:16 +0530

    admin module completed

--- fcadmin/views.py
+++ fcadmin/views.py
@@ -14,3 +14,24 @@ def adminVerifySeller(request):
 
 def adminSuspendedSeller(request):
     return render(request,'adm/admin_suspended_sellers.html')
+
+def adminEditSellerPage(request):
+    return render(request,'adm/admin_edit_sellerpage.html')
+
+def adminViewLogistics(request):
+    return render(request,'adm/admin_view_logistics.html')
+
+def adminVerifyLogistics(request):
+    return render(request,'adm/admin_verify_logistics.html')
+
+def adminSuspendedLogistics(request):
+    return render(request,'adm/admin_suspended_logistics.html')
+
+def adminEditLoisticsPage(request):
+    return render(request,'adm/admin_edit_logisticspage.html')
+
+def adminEditHomePage(request):
+    return render(request,'adm/admin_edit_homepage.html')
+
+def adminChangeCredentials(request):
+    return render(request,'adm/admin_change_credentials.html')
