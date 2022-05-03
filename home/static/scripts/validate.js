386b969c5943e0d27c4f41921184a6188c527e8a
shiblykt <shiblykt56@gmail.com>
2022-05-03 23:34:40 +0530

    validation updates

--- home/static/scripts/validate.js
+++ home/static/scripts/validate.js
@@ -8,7 +8,6 @@ function signup_validate(){
         s = 0
     }else{
         document.getElementById('userFirstName').style.border = '3px solid green'
-        s = 1
     }
 
     lName = document.getElementById('userLastName').value
@@ -17,14 +16,12 @@ function signup_validate(){
         s = 0
     }else{
         document.getElementById('userLastName').style.border = '3px solid green'
-        s = 1
     }
 
     var re =  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
     mail = document.getElementById('userEmail').value
     if(mail.match(re)){
       document.getElementById('userEmail').style.border = '3px solid green'
-      s = 1
     }else{
         document.getElementById('userEmail').style.border = '3px solid red'
         s = 0
@@ -36,7 +33,6 @@ function signup_validate(){
         s = 0
     }else{
         document.getElementById('userMobile').style.border = '3px solid green'
-        s = 1
     }
 
     var rep = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,15}$/
@@ -44,14 +40,12 @@ function signup_validate(){
     passwd2 = document.getElementById('userPassword2').value
     if(passwd.match(rep)){
         document.getElementById('userPassword').style.border = '3px solid green'
-        s = 1
     }else{
         document.getElementById('userPassword').style.border = '3px solid red'
         s = 0
     }
     if(passwd2.match(rep)){
         document.getElementById('userPassword2').style.border = '3px solid green'
-        s = 1
     }else{
         document.getElementById('userPassword2').style.border = '3px solid red'
         s = 0
@@ -67,7 +61,6 @@ function signup_validate(){
         s = 0
     }else{
         document.getElementById('msgpasswd').innerHTML = ''
-        s=1
     }
     
     address = document.getElementById('userAddress').value
@@ -76,7 +69,6 @@ function signup_validate(){
         s = 0
     }else{
         document.getElementById('userAddress').style.border = '3px solid green'
-        s = 1
     }
 
     city = document.getElementById('userCity').value
@@ -85,13 +77,13 @@ function signup_validate(){
         s = 0
     }else{
         document.getElementById('userCity').style.border = '3px solid green'
-        s = 1
     }
+
+
     var r = /^[0-9]{6}$/
     pin = document.getElementById('userPin').value
     if(pin.match(r)){
         document.getElementById('userPin').style.border = '3px solid green'
-        s = 1
     }else{
         document.getElementById('userPin').style.border = '3px solid red'
         s = 0
@@ -101,17 +93,4 @@ function signup_validate(){
         return false
     }
 }
-
-// var stateOptions
-
-//         $.getJSON('indianStates.json',function(){
-//         $.each(function(code,name){
-            
-//         stateOptions+="<Option value='"+code+"'>"+name+"</Option>"
-            
-//         })
-        
-//         $('#stateSelect').html(stateOptions)
-
-//     })
 
